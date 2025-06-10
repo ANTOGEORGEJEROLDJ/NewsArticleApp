83fe1b843558e10698d9c3f7c2166a5ca201048a
Anto George Jerold <antogeorgejeroldj@gmail.com>
2025-06-10 14:46:45 +0530

    - Add loginPage

---
+++ NewsArticle/View/SIgnInScreen/CustomTextField.swift
@@ -0,0 +1,37 @@
+//
+//  CustomTextField.swift
+//  NewsArticle
+//
+//  Created by Paranjothi iOS MacBook Pro on 10/06/25.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    
+    var icon: String
+    var placeHolder: String
+    @Binding var text: String
+    
+    var body: some View {
+        HStack{
+            Image(systemName: icon)
+                .resizable()
+                .frame(width: 30)
+            if placeHolder.lowercased().contains("password") {
+                SecureField(placeHolder, text: $text)
+            } else {
+                TextField(placeHolder, text: $text)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+            }
+        }
+        .padding()
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(10)
+    }
+}
+
+//#Preview {
+//    CustomTextField()
+//}
---
+++ NewsArticle/View/SIgnInScreen/SIgnInScreen.swift
@@ -0,0 +1,115 @@
+//
+//  SIgnInScreen.swift
+//  NewsArticle
+//
+//  Created by Paranjothi iOS MacBook Pro on 10/06/25.
+//
+
+import SwiftUI
+
+struct SIgnInScreen: View {
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @State private var UserName = ""
+    @State private var email = ""
+    @State private var password = ""
+    @State private var navigationToSelectNewsScreen = false
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack {
+                    Image("loginImage")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 175, height: 175)
+                        .padding(.top)
+                    Text("Login to continue")
+                        .padding()
+                        .font(.headline)
+                        .foregroundColor(.black.opacity(0.5))
+                }
+                Group {
+                    VStack(spacing: 20) {
+                        CustomTextField(icon: "person.fill", placeHolder: "UserName", text: $UserName)
+                        CustomTextField(icon: "envelope.fill", placeHolder: "Mail", text: $email)
+                        CustomTextField(icon: "lock.fill", placeHolder: "Password", text: $password)
+                    }
+                    .padding()
+                    .background(Color.white.opacity(0.9))
+                    .cornerRadius(20)
+                    .shadow(radius: 10)
+                    .padding(.horizontal)
+                    .padding(.top, 20)
+                }
+                
+                Spacer()
+                VStack(spacing: 20) {
+                    // NavigationLink to TabView
+                    NavigationLink(
+                        destination: SelectNewsScreen()
+                            .environment(\.managedObjectContext, viewContext),
+                        isActive: $navigationToSelectNewsScreen
+                    ) {
+                        EmptyView()
+                    }
+                    
+                    // Login Button
+                    Button(action: {
+                        navigationToSelectNewsScreen = true // Trigger navigation
+                    }) {
+                        Text("Login")
+                            .frame(width: 258, height: 22)
+                            .padding()
+                            .bold()
+                            .background(Color.blue.opacity(0.2))
+                            .foregroundColor(.black.opacity(0.7))
+                            .font(.headline)
+                            .cornerRadius(15)
+                            .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 5)
+                    }
+                    
+                    HStack(spacing: 20) {
+                        Button(action: {
+                            // Google sign-in action
+                        }) {
+                            Image("google")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 19, height: 19)
+                        }
+                        .frame(width: 70, height: 22)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue.opacity(0.1))
+                        .font(.headline)
+                        .cornerRadius(15)
+                        .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 5)
+                        
+                        Button(action: {
+                            // Apple sign-in action
+                        }) {
+                            Image("appleicon")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 19, height: 19)
+                        }
+                        .frame(width: 70, height: 22)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue.opacity(0.1))
+                        .font(.headline)
+                        .cornerRadius(15)
+                        .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 5)
+                    }
+                }
+                .padding(.top, 50)
+            }
+        }
+    }
+}
+
+#Preview {
+    SIgnInScreen()
+}
